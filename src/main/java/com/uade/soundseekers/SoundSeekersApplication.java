aab42aefe691984498c6840ba25b351a50f9961a
Valentín Campestri <dev.valentincampestri@hotmail.com>
2024-08-20 21:14:00 -0300

    Initial scaffolding

---
+++ src/main/java/com/uade/soundseekers/SoundSeekersApplication.java
@@ -0,0 +1,13 @@
+package com.uade.soundseekers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundSeekersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoundSeekersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uade/soundseekers/SoundSeekersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uade.soundseekers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundSeekersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
