c8d771acf2eb8ba1133c382de3e58a8d193d2214
alqui2002 <alcarazrodrigo1c@gmail.com>
2024-09-16 16:53:14 -0300

    Primer registro y login

---
+++ src/main/java/com/uade/soundseekers/controllers/auth/RegisterRequest.java
@@ -0,0 +1,23 @@
+package com.uade.soundseekers.controllers.auth;
+
+
+
+import com.uade.soundseekers.entity.Role;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+    private Role role;
+}
---
+++ src/main/java/com/uade/soundseekers/service/AuthenticationService.java
@@ -0,0 +1,58 @@
+package com.uade.soundseekers.service;
+
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.uade.soundseekers.controllers.auth.AuthenticationRequest;
+import com.uade.soundseekers.controllers.auth.AuthenticationResponse;
+import com.uade.soundseekers.controllers.auth.RegisterRequest;
+import com.uade.soundseekers.controllers.config.JwtService;
+import com.uade.soundseekers.entity.User;
+import com.uade.soundseekers.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+        private final UserRepository repository;
+        private final PasswordEncoder passwordEncoder;
+        private final JwtService jwtService;
+        private final AuthenticationManager authenticationManager;
+
+        public AuthenticationResponse register(RegisterRequest request) {
+                var user = User.builder()
+                                .firstName(request.getFirstname())
+                                .lastName(request.getLastname())
+                                .email(request.getEmail())
+                                .password(passwordEncoder.encode(request.getPassword()))
+                                .role(request.getRole())
+                                .build();
+
+                repository.save(user);
+                var jwtToken = jwtService.generateToken(user);
+                return AuthenticationResponse.builder()
+                                .accessToken(jwtToken)
+                                .build();
+        }
+
+        public AuthenticationResponse authenticate(AuthenticationRequest request) {
+                authenticationManager.authenticate(
+                                new UsernamePasswordAuthenticationToken(
+                                                request.getEmail(),
+                                                request.getPassword()));
+
+                var user = repository.findByEmail(request.getEmail())
+                                .orElseThrow();
+                var jwtToken = jwtService.generateToken(user);
+                return AuthenticationResponse.builder()
+                                .accessToken(jwtToken)
+                                .build();
+        }
+}
+
