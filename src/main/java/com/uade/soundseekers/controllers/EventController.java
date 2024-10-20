2dcfeb8c8d0a39f4cb58cf7726cb1d39a37fd4e5
Valentín Campestri <dev.valentincampestri@hotmail.com>
2024-10-20 20:28:11 -0300

    import

--- src/main/java/com/uade/soundseekers/controllers/EventController.java
+++ src/main/java/com/uade/soundseekers/controllers/EventController.java
@@ -6,7 +6,6 @@ import com.uade.soundseekers.entity.Image;
 import com.uade.soundseekers.entity.MusicGenre;
 import com.uade.soundseekers.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
