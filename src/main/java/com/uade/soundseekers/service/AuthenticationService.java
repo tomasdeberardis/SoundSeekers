cc0aef703f4cc79bc5ac283660a66d6eb26a43ca
Valentín Campestri <dev.valentincampestri@hotmail.com>
2024-10-20 21:30:33 -0300

    Fer

--- src/main/java/com/uade/soundseekers/service/AuthenticationService.java
+++ src/main/java/com/uade/soundseekers/service/AuthenticationService.java
@@ -85,8 +85,8 @@ public class AuthenticationService {
 
         var user = repository.findByEmail(request.getEmail()).orElseThrow();
         var jwtToken = jwtService.generateToken(user);
-        return AuthenticationResponse.builder().
-            accessToken(jwtToken)
+        return AuthenticationResponse.builder()
+            .accessToken(jwtToken)
             .role(user.getRole().name())
             .userId(user.getId())
             .build();
