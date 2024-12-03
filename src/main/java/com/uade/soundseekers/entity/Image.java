2bbf354af09a8ed8e4dbd1911c1fd76aa2dc23f7
Twave22 <108684601+tomasdeberardis@users.noreply.github.com>
2024-12-03 15:11:43 -0300

    Revert "Feature/imagen"

--- src/main/java/com/uade/soundseekers/entity/Image.java
+++ src/main/java/com/uade/soundseekers/entity/Image.java
@@ -1,6 +1,5 @@
 package com.uade.soundseekers.entity;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -10,26 +9,57 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
 @Entity
 @Table(name = "Image")
 public class Image {
 
+    @Getter
+    @Setter
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String path;
+    private int id;
     private String imageName;
+
+
+    @Getter
     private String description;
 
+    @Setter
+    @Getter
     @Lob
     @Column(columnDefinition = "LONGBLOB")
     private byte[] imageData;
 
+    @Setter
+    @Getter
     @ManyToOne
-    @JoinColumn(name = "event_id")
-    @JsonBackReference
+    @JoinColumn(name="event_id")
     private Event event;
+
+    public Image() {
+
+    }
+
+    public Image(byte[] imageData) {
+        super();
+        this.imageData = imageData;
+    }
+
+    public String imageName() {
+        return imageName;
+    }
+
+    public void imageName(String imageName) {
+        this.imageName = imageName;
+    }
+
+    @Override
+    public String toString() {
+        return "Image [id=" + id + ", imageName=" + imageName  + ", description=" + description
+                + ", Event=" + event + "]";
+    }
+
 }
